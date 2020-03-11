a195e04e62f6f5d5fe3187ad58afa0e45d43e671
BedirT <tapkan@ualberta.ca>
2020-03-11 12:37:08 -0600

    setup completed

---
+++ _site/assets/js/search-data.js
@@ -0,0 +1,26 @@
+var store = [{
+        "title": "Machine Learning Basics",
+        "excerpt":"Reaaally introductory post for Machine Learning basics. What is Supervised and Unsupervised learning? Supervised Learning Supervised learning is the most common machine learning problem. In Supervised Learning we already know what the correct output should be like. There are two categories for supervised learning, first one is “regression problem” and...","categories": ["Machine Learning"],
+        "tags": [],
+        "url": "http://localhost:4000/machine%20learning/2016/02/27/Machine-Learning-Basics.html"
+      },{
+        "title": "Kadane's Algorithm",
+        "excerpt":"What we will learn today is, how to find the sum of maximum of the subarrays, in optimal solution. First, let’s clarify the goal a bit. What is subarray? Subarray is an array that is included in the bigger array. So if we have an array that has 7 elements...","categories": ["Algorithms"],
+        "tags": ["Competitive programming"],
+        "url": "http://localhost:4000/algorithms/2016/09/18/Kadane's-Algorithm.html"
+      },{
+        "title": "Factorizing a Number",
+        "excerpt":"Before starting- What is Prime Factorization ? What is a Prime number ? If you are curious about these please checkout this link before proceed because I will not explain them here :) Wikipedia - Prime Numbers Since we all know what a prime number and composite number is, let’s...","categories": ["Algorithms"],
+        "tags": ["Competitive programming"],
+        "url": "http://localhost:4000/algorithms/2016/11/20/Prime-Number-Factorization.html"
+      },{
+        "title": "Counting Sort",
+        "excerpt":"Counting sort is a nice in-place sorting algorithm that we can use for sorting instantly. (This is mostly used for competitive programming.) What I meant by this is, we can use counting sort when we are getting the input. This will not get any additional cost for us, and really...","categories": ["Algorithms"],
+        "tags": ["Competitive programming"],
+        "url": "http://localhost:4000/algorithms/2017/02/01/counting-sort.html"
+      },{
+        "title": "Semi-Gradient Control Methods",
+        "excerpt":"Prerequisites Semi-Gradient Prediction Intro to Linear MethodsIf you read the prediction part for the semi gradient methods, it is pretty easy to extend what we know to the control case. We know that control is almost all the time just adding policy improvement over the prediction case. That’s exactly the...","categories": ["Reinforcement Learning","Control Methods"],
+        "tags": ["SARSA","BetterRL Series"],
+        "url": "http://localhost:4000/reinforcement%20learning/control%20methods/2020/03/10/Semi-Gradient-Control.html"
+      },]
